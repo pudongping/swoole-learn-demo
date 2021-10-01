f45dcde211e426c41e28f59d016d4cd7696bb845
pudongping <276558492@qq.com>
2021-10-01 20:19:38 +0800

    taskCo 并发执行 task 并进行协程调度

--- taskWaitMulti_and_taskCo_usage/README.md
+++ taskWaitMulti_and_taskCo_usage/README.md
@@ -43,5 +43,40 @@ hello
 
 并发执行 task 并进行协程调度
 
+### 服务端
+
+```shell
+
+root@dc705af7d5da:/var/www/swoole-learn-demo/taskWaitMulti_and_taskCo_usage# php taskCo.php
+task ====> 1
+task ====> 2
+array(3) {
+  [1]=>
+  string(8) "success2"
+  [0]=>
+  string(8) "success1"
+  [2]=>
+  bool(false)
+}
+hello Alex!
+task ====> 3
+[2021-10-01 12:12:57 *582.0]	WARNING	php_swoole_server_onFinish() (ERRNO 2003): task[2] has expired
+
+
+```
+
+### 客户端
+
+```shell
+
+telnet 127.0.0.1:5200
+
+Trying 127.0.0.1...
+Connected to localhost.
+Escape character is '^]'.
+hello
+
+```
+
 > 二者区别在于 taskWaitMulti 主进程等待
 
--- taskWaitMulti_and_taskCo_usage/taskCo.php
+++ taskWaitMulti_and_taskCo_usage/taskCo.php
@@ -1,6 +1,6 @@
 <?php
 /**
- *
+ * 并发执行 task 并进行协程调度
  *
  * Created by PhpStorm
  * User: Alex
@@ -19,7 +19,11 @@ $server->set([
 $server->on('receive', function ($serv, $fd, $reactorId, $data) {
 
     // 投递任务到 task 进程中
-    $serv->task('task data ====> ' . $data);
+    // 因为总共设置的超时时间是 5 秒，然而协程执行的时候会在第一个任务中消耗 3 秒
+    // 第二个任务也消耗 3 秒，因此第三个任务就直接会执行失败
+    $res = $serv->taskCo([1, 2, 3], 5);
+
+    var_dump($res);
 
     echo 'hello Alex!' . PHP_EOL;  // 不会阻塞，会立刻向下执行
 
@@ -27,12 +31,12 @@ $server->on('receive', function ($serv, $fd, $reactorId, $data) {
 
 $server->on('task', function ($serv, $taskId, $srcWorker, $data) {
 
-    sleep(5);  // 模拟耗时任务
+    sleep(3);  // 模拟耗时任务
 
     // 接收到了任务
     echo 'task ====> ' . $data . PHP_EOL;
 
-    return 'success';  // 将异步任务处理结果返回出去，以便 onFinish 回调函数可以接收
+    return 'success' . $data;  // 将异步任务处理结果返回出去，以便 onFinish 回调函数可以接收
 
 });
 
